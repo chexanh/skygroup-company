a771c5e52d4c6e93c0a91bf3b9b6b14aac252674
chexanh <luu_ducdinh_na@yahoo.com>
2021-06-14 01:38:02 +0900

    最初のコミット

---
+++ mail.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P4VV6G6');</script>
+<!-- End Google Tag Manager -->
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"> -->
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>不用品回収なら、どこよりも格安なスカイリサイクル</title>
+<meta name="description" content="不用品回収なら、どこよりも格安なスカイリサイクル。不用品回収最安値に挑戦中。他社様に比べて1円でも高い場合はご相談ください。ご状況に合わせてお安く最適なプランをご提案いたします。軽トラ乗せ放題パック￥9,800。1日3組様限定です。" />
+<meta name="keywords" content="不用品回収,粗大ごみ回収,ゴミ屋敷清掃,スカイリサイクル" />
+
+<!-- ogp -->
+<meta property="og:title" content="不用品回収なら、どこよりも格安なスカイリサイクル" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="不用品回収なら、どこよりも格安なスカイリサイクル。不用品回収最安値に挑戦中。他社様に比べて1円でも高い場合はご相談ください。ご状況に合わせてお安く最適なプランをご提案いたします。軽トラ乗せ放題パック￥9,800。1日3組様限定です。" />
+<meta property="og:site_name" content="不用品回収なら、どこよりも格安なスカイリサイクル" />
+<meta property="og:image" content="img/ogp.jpg" />
+<!-- //ogp -->
+
+<link rel="stylesheet" href="css/style.css?20191008">
+<script src="js/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4VV6G6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!-- header -->
+<div id="header" class="header">
+<div class="hti">
+<h1 class="pc">不用品回収なら、どこよりも格安なスカイリサイクル</h1>
+</div>
+<div class="wmain">
+<div class="logo left">
+<a href="./"><img src="img/logo_pc.png" alt="スカイリサイクル" class="switch"></a>
+</div>
+<div class="head-cta right">
+<ul>
+<li><a href="./#area12"><img src="img/hd-mail_pc.png" alt="無料相談はこちら" class="switch"></a></li>
+<li><a href="" target="_blank"><img src="img/hd-line_pc.png" alt="" class="switch"></a></li>
+<li class="tel"><a href="tel:0120-686-724" onclick="javascript:goog_report_conversion('tel:0120-686-724');yahoo_report_conversion(undefined);return false;"><img src="img/hd-tel_pc.png" alt="tel.0120-686-724" class="switch"></a></li>
+</ul>
+<div class="reflection"></div>
+</div>
+</div>
+</div>
+
+<div class="contact confirm">
+<div class="wmain">
+<!-- フォーム -->
+<div id="formWrap">
+<div class="inwrap w922">
+<p class="error">入力にエラーがあります。下記をご確認の上<br class="sp" />「戻る」ボタンにて修正をお願い致します。</p>
+<div class="formwrap">
+<div align="center">
+<p class="error_messe">【お名前】は必須項目です。</p>
+<p class="error_messe">【メールアドレス】は必須項目です。</p>
+<p class="error_messe">【電話番号】は必須項目です。</p>
+<p class="error_messe">【都道府県】は必須項目です。</p>
+<p class="error_messe">【市区町村】は必須項目です。</p>
+<p class="error_messe">【ご相談内容】は必須項目です。</p>
+</div>
+</div>
+<div class="text-center MB20">
+<input type="button" value="前画面に戻る" class="form-btn-back form-btn" onClick="history.back()" >
+</div> 
+
+</div>
+</div>
+</div><!-- /formWrap -->
+</div>
+</div>
+
+<!-- footer -->
+<div class="footer">
+<div class="wmain">
+<h2>スカイリサイクル</h2>
+<p>〒652-0897 神戸市兵庫区駅南通4丁目4-13<br>Tel. 0120-686-724</p>
+</div>
+<div class="copyright">
+<p>Copyright© スカイリサイクル <br class="sp">All Rights Reserved.</p>
+</div>
+<p class="gotop"><a href="#header"><img src="img/gotop.png" alt="" title=""></a></p>
+</div>
+
+</body>
+</html>
